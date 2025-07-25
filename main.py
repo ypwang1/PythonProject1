7ef52a535f4c8630d61270bbc592f576b3e2da14
Yupeng Wang <yupengori@gmail.com>
2025-07-25 13:02:54 +0200

    update

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ This will install the packages from requirements.txt for this project.
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-Bootstrap5(app)
+Bootstrap5(app
 
 # CREATE DB
 
